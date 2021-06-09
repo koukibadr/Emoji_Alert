1c6311b798effc211c4e8752c0579d3da86ffc39
Kouki Badr <badreddinekouki1998@gmail.com>
2021-06-09 07:38:59 +0100

    add example projet

--- lib/emoji_alert.dart
+++ lib/emoji_alert.dart
@@ -1,7 +1,43 @@
-library emoji_alert;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class EmojiAlert extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      color: Color(0x00000000),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 150,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  )),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Image(image: AssetImage("assets/images/happy.png", package: 'emoji_alert'),width: 70,),
+          ),
+        ],
+      ),
+    );
+  }
+
+  show(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: this,
+            backgroundColor: Colors.transparent,
+            contentPadding: EdgeInsets.all(0),
+          );
+        });
+  }
 }
