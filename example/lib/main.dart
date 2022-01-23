b3a4c403503279c213c00219c7f4c96ab2a922b0
Kouki Badr <badreddinekouki1998@gmail.com>
2022-01-23 10:03:54 +0100

    update package example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,66 +26,76 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Simple example with miniumum parameters"),
-              ElevatedButton(
-                  onPressed: () {
-                    displayAlert(context);
-                  },
-                  child: Text("Happy Alert")),
-              SizedBox(
-                height: 20,
-              ),
-              Text("Alert with custom body widgets (textfield)"),
-              ElevatedButton(
-                  onPressed: () {
-                    displayShockedAlert(context);
-                  },
-                  child: Text("Joyful Alert")),
-              SizedBox(
-                height: 20,
-              ),
-              Text("Alert with main button example with press handler"),
-              ElevatedButton(
-                  onPressed: () {
-                    displayAngryAlert(context);
-                  },
-                  child: Text("Angry Alert")),
-              SizedBox(
-                height: 20,
-              ),
-              Text("Alert with multiple buttons example"),
-              ElevatedButton(
-                  onPressed: () {
-                    displayCoolAlert(context);
-                  },
-                  child: Text("Multiple buttons Alert")),
-              SizedBox(
-                height: 20,
-              ),
-              Text("Emoji alert displayed as a bottom sheet modal"),
-              ElevatedButton(
-                  onPressed: () {
-                    displayBottomSheetAlert(context);
-                  },
-                  child: Text("Bottom Sheet Alert")),
-            ],
-          )),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Simple example with miniumum parameters'),
+            ElevatedButton(
+              onPressed: () {
+                displayAlert(context);
+              },
+              child: Text('Happy Alert'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Alert with custom body widgets (textfield)'),
+            ElevatedButton(
+              onPressed: () {
+                displayShockedAlert(context);
+              },
+              child: Text('Joyful Alert'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Alert with main button example with press handler'),
+            ElevatedButton(
+              onPressed: () {
+                displayAngryAlert(context);
+              },
+              child: Text('Angry Alert'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Alert with multiple buttons example'),
+            ElevatedButton(
+              onPressed: () {
+                displayCoolAlert(context);
+              },
+              child: Text('Multiple buttons Alert'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Emoji alert displayed as a bottom sheet modal'),
+            ElevatedButton(
+              onPressed: () {
+                displayBottomSheetAlert(context);
+              },
+              child: Text('Bottom Sheet Alert'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  displayAlert(context) {
+  void displayAlert(context) {
     EmojiAlert(
-      alertTitle:
-          Text("Emoji Alert", style: TextStyle(fontWeight: FontWeight.bold)),
+      alertTitle: Text(
+        'Emoji Alert',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
       height: 250,
       description: Column(
         children: [
-          Text("Simple Example with animation"),
-          Text("This is a transition animation"),
+          Text('Simple Example with animation'),
+          Text('This is a transition animation'),
         ],
       ),
       enableMainButton: true,
@@ -93,18 +103,23 @@ class HomeScreen extends StatelessWidget {
     ).displayAlert(context);
   }
 
-  displayShockedAlert(context) {
+  void displayShockedAlert(context) {
     EmojiAlert(
-      alertTitle:
-          Text("Joyful Alert", style: TextStyle(fontWeight: FontWeight.bold)),
+      alertTitle: Text(
+        'Joyful Alert',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
       description: Column(
         children: [
-          Text("You can add also any widget"),
+          Text('You can add also any widget'),
           Container(
-              width: 200,
-              child: TextField(
-                decoration: InputDecoration(hintText: "Input field example"),
-              )),
+            width: 200,
+            child: TextField(
+              decoration: InputDecoration(hintText: 'Input field example'),
+            ),
+          ),
         ],
       ),
       cancelable: true,
@@ -113,19 +128,22 @@ class HomeScreen extends StatelessWidget {
     ).displayAlert(context);
   }
 
-  displayAngryAlert(context) {
+  void displayAngryAlert(context) {
     EmojiAlert(
-      alertTitle:
-          Text("Angry Alert", style: TextStyle(fontWeight: FontWeight.bold)),
+      alertTitle: Text(
+        'Angry Alert',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
       description: Column(
         children: [
-          Text("You can add a main button"),
+          Text('You can add a main button'),
         ],
       ),
       enableMainButton: true,
       mainButtonColor: Colors.red,
       onMainButtonPressed: () {
-        print("Hello");
         Navigator.pop(context);
       },
       cancelable: false,
@@ -134,36 +152,48 @@ class HomeScreen extends StatelessWidget {
     ).displayAlert(context);
   }
 
-  displayCoolAlert(context) {
+  void displayCoolAlert(context) {
     EmojiAlert(
-      alertTitle:
-          Text("Cool Alert", style: TextStyle(fontWeight: FontWeight.bold)),
+      alertTitle: Text(
+        'Cool Alert',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
       description: Column(
         children: [
-          Text("You can add multiple buttons",
-              style: TextStyle(color: Colors.blue)),
+          Text(
+            'You can add multiple buttons',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          ),
         ],
       ),
       enableMainButton: true,
       enableSecondaryButton: true,
       mainButtonColor: Colors.green,
       secondaryButtonColor: Colors.green,
-      mainButtonText: Text("Save"),
-      secondaryButtonText: Text("Discard"),
+      mainButtonText: Text('Save'),
+      secondaryButtonText: Text('Discard'),
       cancelable: true,
       emojiType: EMOJI_TYPE.cool,
       height: 300,
     ).displayAlert(context);
   }
 
-  displayBottomSheetAlert(context) {
+  void displayBottomSheetAlert(context) {
     EmojiAlert(
-      alertTitle: Text("Bottom Sheet Modal",
-          style: TextStyle(fontWeight: FontWeight.bold)),
+      alertTitle: Text(
+        'Bottom Sheet Modal',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
       description: Column(
         children: [
           Text(
-            "And this is a bottom sheet modal with different Radius",
+            'And this is a bottom sheet modal with different Radius',
             style: TextStyle(
               color: Colors.blue,
             ),
@@ -174,7 +204,7 @@ class HomeScreen extends StatelessWidget {
       enableMainButton: true,
       cornerRadiusType: CORNER_RADIUS_TYPES.topOnly,
       mainButtonColor: Colors.green,
-      mainButtonText: Text("Save"),
+      mainButtonText: Text('Save'),
       onMainButtonPressed: () {
         Navigator.pop(context);
         displayAngryAlert(context);
